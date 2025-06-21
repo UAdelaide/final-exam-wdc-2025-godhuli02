c3d62fce124dfac857df3e1295835025deb447c1
godhuli02 <godhuliazad02@gmail.com>
2025-06-21 07:20:16 +0000

    [automatic commit] 2025-06-21 07:20:16 UTC 101.115.226.251

--- part1/app.js
+++ part1/app.js
@@ -59,7 +59,8 @@ async function setupDB() {
 
       for (const [dogName, date_time, duration, location, status] of walkData) {
         await db.execute(
-          `INSERT INTO WalkRequests (dog_id, date_time, duration_minutes, location, status)
+          `INSERT INTO WalkRequests (dog_id, datetime, duration_minutes, location, status)
+
            VALUES (
              (SELECT dog_id FROM Dogs WHERE name = ?),
              ?, ?, ?, ?
